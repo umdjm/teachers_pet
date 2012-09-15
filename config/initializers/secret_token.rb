911e99e8480aca2ae818557af9f599607b095aff
Jamie Wright <jamie@brilliantfantastic.com>
2012-09-14 22:46:41 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TeachersPet::Application.config.secret_token = 'f2328ef929972f103179d6958236244380b67de23e985457a9e3e7da2a74d8c53de1c1429ded8b37d3d188ac3c4b1fd70b2a3c463cbc5e044ca76054cf7d569e'

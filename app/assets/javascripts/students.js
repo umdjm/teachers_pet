accfc44273cb6b8308c17d7c1d183af4def5d38b
Don Miller <donmiller@Don-Miller-MBP-Retina.local>
2012-09-16 14:20:22 -0400

    Added DJs changes

--- app/assets/javascripts/students.js
+++ app/assets/javascripts/students.js
@@ -1,13 +1,10 @@
 var availableTags = [];
 
 $(document).ready(function() {
-
     $.each( $("#UngradedTable tr td"), function(i, td){
         availableTags.push($(td).html().trim());
-      });
-});
+    });
 
-$(function() {
     $( "#studentName" ).autocomplete({
         autoFocus: true,
         delay: 0,
@@ -15,22 +12,18 @@ $(function() {
     });
 });
 
+
 $(function() {
     $("#score").keyup(function(event){
-        if(event.keyCode >= 48 && event.keyCode)
-        {
-            $('#score').focus();
-            $('#score').val() = 12;
-        }
-        else if(event.keyCode == 13){
+        if(event.keyCode == 13){
+
             var $studentName = $('#studentName').val();
-            var $score = $('#score').val();
+            var score = $('#score').val();
             var $old = $('#UngradedTable tr').find(':contains("'+ $studentName +'")').parent();
             //First we copy the arrow to the new table cell and get the offset to the document
             var $new = $old.clone().appendTo('#GradedTable');
             var newOffset = $new.offset();
             var oldOffset = $old.offset();
-            $new.append("<td>"+ $score +"</td>");
             var $temp = $new.clone().appendTo('body');
             $new.hide();
             $old.hide();
@@ -43,12 +36,27 @@ $(function() {
 
             $temp.animate( {'top': newOffset.top, 'left':newOffset.left}, 'slow', function(){
                 //callback function, we remove $old and $temp and show $new
+                $new.append("<td>"+ score +"</td>");
+                $new.removeClass("ungradedStudent");
                 $new.show();
                 $old.remove();
                 $temp.remove();
             });
             $('#studentName').focus().val("");
             $('#score').val("");
+
+            var index = availableTags.indexOf($studentName);
+            availableTags.splice(index, 1);
+            $( "#studentName" ).autocomplete({
+                autoFocus: true,
+                delay: 0,
+                source: availableTags
+            });
+
+            var doneCount = $("#GradedTable tr").length - 1;
+            var undoneCount = $("#UngradedTable tr").length - 1;
+            var percentComplete = Math.round((100*doneCount)/((undoneCount+doneCount)));
+            $("#completion").html(percentComplete + "% Complete");
 
         }
     });
